d9249f302952f544a9dac170d4bdce9de3786367
Unknown <nawrocki.iakub@gmail.com>
2018-10-17 16:16:41 +0200

    Working listview
    Reworked deserialization so it works correctly. Now it deserializes to a single object, and inside that object is a LIST of properties, which are the actual objects we want to call.

--- jsonlistview/jsonlistview/MainPage.xaml.cs
+++ jsonlistview/jsonlistview/MainPage.xaml.cs
@@ -5,20 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using System.Linq;
+using System.Collections.ObjectModel;
+using static jsonlistview.ToiletCollection;
 
 namespace jsonlistview
 {
+
     public partial class MainPage : ContentPage
     {
+        readonly IList<RootObject> toilets = new ObservableCollection<RootObject>();
+        //readonly ToiletManager manager = new ToiletManager();
         public MainPage()
         {
             InitializeComponent();
 
             ToiletCollection tc = new ToiletCollection();
-            tc.GetData();
-            Label label = new Label { Text = "testing", BackgroundColor = Color.White, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, TextColor = Color.Black };
-            Content = label;
-           // System.Diagnostics.Debug.WriteLine(list);
+            var toilets = tc.GetData();
+            BindingContext = toilets.features;
+
+            
+            // foreach(var item in toilets)
+            // {
+            ////     System.Diagnostics.Debug.WriteLine(item.features[0].properties.Navn);
+            // }
+            System.Diagnostics.Debug.WriteLine(toilets.features[0].properties.Navn);
+            System.Diagnostics.Debug.WriteLine(toilets.features[1].properties.Navn);
+            System.Diagnostics.Debug.WriteLine(toilets.features[2].properties.Navn);
         }
+        public void ItemTapped(object sender, EventArgs e) { }
+        public void ItemDelete(object sender, EventArgs e) { }
     }
+    
 }
--- jsonlistview/jsonlistview/ToiletCollection.cs
+++ jsonlistview/jsonlistview/ToiletCollection.cs
@@ -7,75 +7,87 @@ using System.Net;
 using System.Reflection;
 using System.Text;
 using System.Xml.Linq;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
 
 namespace jsonlistview
 {
+
+    [JsonObject]
+    public class RootObject
+    {
+        public string type { get; set; }
+        [JsonProperty("features")]
+        public List<Feature> features { get; set; }
+        [JsonProperty("crs")]
+        public Crs crs { get; set; }
+        [JsonProperty("bbox")]
+        public List<double> bbox { get; set; }
+    }
+    public class Geometry
+    {
+        public string type { get; set; }
+        [JsonProperty("coordinates")]
+        public List<double> coordinates { get; set; }
+    }
+
+    public class Properties
+    {
+        public string Navn { get; set; }
+        public string Adresse { get; set; }
+        public string Beskrivelse { get; set; }
+        public string Pris { get; set; }
+        public string Type { get; set; }
+        public string Inaktiv { get; set; }
+    }
+    [JsonObject]
+    public class Feature
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+        [JsonProperty("geometry")]
+        public Geometry geometry { get; set; }
+        public string geometry_name { get; set; }
+        [JsonProperty("properties")]
+        public Properties properties { get; set; }
+    }
+
+    public class Properties2
+    {
+        public string code { get; set; }
+    }
+
+    public class Crs
+    {
+        public string type { get; set; }
+        [JsonProperty("properties2")]
+        public Properties2 properties { get; set; }
+    }
+
     public class ToiletCollection
     {
-        public class Geometry
-        {
-            public string type { get; set; }
-            public List<double> coordinates { get; set; }
-        }
-
-        public class Properties
-        {
-            public string Navn { get; set; }
-            public string Adresse { get; set; }
-            public string Beskrivelse { get; set; }
-            public string Pris { get; set; }
-            public string Type { get; set; }
-            public string Inaktiv { get; set; }
-        }
-
-        public class Feature
-        {
-            public string type { get; set; }
-            public string id { get; set; }
-            public Geometry geometry { get; set; }
-            public string geometry_name { get; set; }
-            public Properties properties { get; set; }
-        }
-
-        public class Properties2
-        {
-            public string code { get; set; }
-        }
-
-        public class Crs
-        {
-            public string type { get; set; }
-            public Properties2 properties { get; set; }
-        }
-
-        public class RootObject
-        {
-            public string type { get; set; }
-            public List<Feature> features { get; set; }
-            public Crs crs { get; set; }
-            public List<double> bbox { get; set; }
-        }
 
         public ToiletCollection()
         {
         }
 
-        public void GetData()
+        public RootObject GetData()
         {
             //WebClient is disposable
             using (WebClient wc = new WebClient())
             {
                 var json = wc.DownloadString("https://portal.opendata.dk/dataset/99e2a056-3350-4f6d-ae5f-77766a2c7011/resource/50c64627-2bde-4406-a958-8f45237b1eb6/download/hctoiletterodense.json");
-                //using online version because xamarin cant find the local file
-                var ts = JsonConvert.DeserializeObject<RootObject>(json);
+                //using online version because xamarin cant find the local file 
                 //deserialize root object which contains all other properties
+                var data = JsonConvert.DeserializeObject<RootObject>(json);
+                IEnumerable<Feature> list = data.features;
                 System.Diagnostics.Debug.WriteLine("------------------------------------------");
                 System.Diagnostics.Debug.WriteLine("------------------------------------------");
                 System.Diagnostics.Debug.WriteLine("------------------------------------------");
-                System.Diagnostics.Debug.WriteLine(ts.features[0].geometry_name);
-                System.Diagnostics.Debug.WriteLine("------------------------------------------");
-                System.Diagnostics.Debug.WriteLine("------------------------------------------");
-                System.Diagnostics.Debug.WriteLine("------------------------------------------");
+                System.Diagnostics.Debug.WriteLine(list.GetType()); 
+                return data;
+                // return ts.AsEnumerable();
             }
         }
     }
